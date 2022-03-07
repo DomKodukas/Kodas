8d500cf8da729960437acbd8e15176dff1197626
DomKodukas <dominykas.pugacius@stud.svako.lt>
2022-03-07 15:32:56 +0200

    Add project files.

---
+++ Kodas/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matke
+{
+    internal class klase
+    {
+        public double b;
+        public int kiek;
+        public void Sk()
+        {
+            kiek = 2;
+            double isvedimas = 0;
+            double a = 1;
+            b = 2;
+            double galas = Math.Pow(10, 15);
+            while (b <= galas)
+            {
+                kiek++;
+                isvedimas = a * b;
+                a = b;
+                b = isvedimas;
+            }
+        }
+    }
+
+}
---
+++ Kodas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+namespace testas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            matke.klase myObj = new matke.klase();
+            myObj.Sk();
+            Console.WriteLine("Elementu kiekis : " + myObj.kiek);
+            Console.WriteLine("Atsakymas : " + myObj.b);
+
+            DateTime start = DateTime.Now;
+            Thread.Sleep(0);
+            DateTime end = DateTime.Now;
+
+            TimeSpan ts = (end - start);
+            Console.WriteLine("Praejas laikas {0} ms", ts.TotalMilliseconds);
+            while (true) ;
+        }
+    }
+}
