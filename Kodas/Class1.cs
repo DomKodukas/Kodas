95f8f396150a3d8667c075ef21f3cae6ea60c250
DomKodukas <dominykas.pugacius@stud.svako.lt>
2022-03-07 15:34:47 +0200

    pakeista

--- Kodas/Class1.cs
+++ Kodas/Class1.cs
@@ -8,9 +8,11 @@ namespace matke
 {
     internal class klase
     {
-        public double b;
-        public int kiek;
-        public void Sk()
+        public double b;//SUKURTAS KINTAMASIS
+        public int kiek;//SUKURTAS KINTAMASIS
+        public int i, fakt = 1, skaicius = 5;//SUKURTAS KINTAMASIS
+        public klase()
+        //ELEMENTO SKAICIAVIMAS
         {
             kiek = 2;
             double isvedimas = 0;
@@ -23,6 +25,11 @@ namespace matke
                 isvedimas = a * b;
                 a = b;
                 b = isvedimas;
+            }
+            //FAKTORIALO SKAICIAVIMO CIKLIUKAS
+            for (i = 1; i <= skaicius; i++)
+            {
+                fakt = fakt * i;
             }
         }
     }
--- Kodas/Program.cs
+++ Kodas/Program.cs
@@ -4,19 +4,23 @@ namespace testas
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             matke.klase myObj = new matke.klase();
-            myObj.Sk();
-            Console.WriteLine("Elementu kiekis : " + myObj.kiek);
-            Console.WriteLine("Atsakymas : " + myObj.b);
 
+            Console.WriteLine("Elementu kiekis : " + myObj.kiek);//ELEMENTO KIEKIO SPAUSDINIMAS
+            Console.WriteLine("Atsakymas : " + myObj.b);//SUSKAICIUOTO ATSAKYMO SPAUSDINIMAS
+            Console.WriteLine();//TARPAS TARP EILUCIU
+            Console.WriteLine("Faktorialas is " + myObj.skaicius + " yra : " + myObj.fakt);//FATORIALO SPAUSDINIMAS
+
+            Console.WriteLine();//TARPAS TARP EILUCIU
+            //DEBUG KODAS KURIS DUOS SUPRASTI PER KIEK LAIKO KODAS SUVEIKE
             DateTime start = DateTime.Now;
             Thread.Sleep(0);
             DateTime end = DateTime.Now;
 
             TimeSpan ts = (end - start);
-            Console.WriteLine("Praejas laikas {0} ms", ts.TotalMilliseconds);
+            Console.WriteLine("Praejas laikas {0} ms", ts.TotalMilliseconds);//SPAUSDINAMAS ATSAKYMAS
             while (true) ;
         }
     }
